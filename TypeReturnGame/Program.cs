dfb2ae7cf6b0c08ea17da77e0899e0b5e5271129
David Gil de Gómez Pérez <david@studiosi.es>
2018-02-18 00:32:51 +0200

    Game Completed

--- TypeReturnGame/Program.cs
+++ TypeReturnGame/Program.cs
@@ -8,8 +8,54 @@ namespace TypeReturnGame
 {
     class Program
     {
+
+        const int MAX_FAILS = 8;
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("TYPE RETURN, a game by Studiosi");
+            Console.WriteLine("You are a desperate programmer, trying to exit a function desperately... TYPE RETURN!");
+            Console.WriteLine("(Note: Your keyboard might be broken)");
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+            Console.Clear();
+
+            // Changing the mapping of the characters
+            ReturnChecker rc = new ReturnChecker();
+            rc.generateEncoding();
+
+            int fails = MAX_FAILS;
+            while(fails > 0)
+            {
+                Console.WriteLine(String.Format("Iterations left: {0}", fails));
+                Console.Write("> ");
+                string inStr = Console.ReadLine();
+                inStr = inStr.ToUpper();
+                string translatedStr = rc.translateString(inStr);
+                RETURN_TYPE ret = rc.checkString(translatedStr);
+                if(ret == RETURN_TYPE.SUCCESS)
+                {
+                    Console.WriteLine("Exiting! Success!");                    
+                    break;
+                }                  
+                else if(ret == RETURN_TYPE.ERROR_LENGTH)
+                {
+                    Console.WriteLine("SYNTAX ERROR: Command exceeded maximum length");
+                    fails--;
+                }
+                else if(ret == RETURN_TYPE.ERROR)
+                {
+                    Console.WriteLine("Executing command... " + translatedStr);
+                    Console.WriteLine("SYNTAX ERROR");
+                    fails--;
+                }
+            }
+            if(fails == 0)
+            {
+                Console.WriteLine("You did not succeed... now the whole world is destroyed.");
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ TypeReturnGame/ReturnChecker.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeReturnGame
+{ 
+
+    public enum RETURN_TYPE
+    {
+        SUCCESS,
+        ERROR_LENGTH,
+        ERROR
+    }
+
+    class ReturnChecker
+    {
+
+        const int Idx_R = 0;
+        const int Idx_E = 1;
+        const int Idx_T = 2;
+        const int Idx_U = 3;
+        const int Idx_N = 4;
+
+        const string okString = "RETURN";
+
+        public char[] chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        // RETURN contains RETUN Letters
+
+        public char[] encoding;
+        private Random r;
+
+        public ReturnChecker()
+        {
+            encoding = new char[5];
+            r = new Random();
+        }
+
+        public void generateEncoding()
+        {
+            List<int> alreadyUsedIdx = new List<int>();
+            encoding = new char[5];
+            
+            for(int i = 0; i < 5; i++)
+            {
+                int charId = r.Next(0, chars.Length);
+                while(alreadyUsedIdx.Contains(charId))
+                {
+                    charId = r.Next(0, chars.Length);
+                }
+                alreadyUsedIdx.Add(charId);
+                encoding[i] = chars[charId];
+            }
+
+        }
+
+        public string translateString(string inStr)
+        {
+            if(okString.Equals(inStr))
+            {
+                return "XXXXXX";
+            }
+            string retStr = "";
+            foreach(char c in inStr)
+            {
+                if(c == encoding[Idx_R])
+                {
+                    retStr += 'R';
+                }
+                else if(c == encoding[Idx_E])
+                {
+                    retStr += 'E';
+                }
+                else if(c == encoding[Idx_T])
+                {
+                    retStr += 'T';
+                }
+                else if(c == encoding[Idx_U])
+                {
+                    retStr += 'U';
+                }
+                else if(c == encoding[Idx_N])
+                {
+                    retStr += 'N';
+                }
+                else
+                {
+                    retStr += c;
+                }
+            }
+            return retStr;
+        }
+
+        public RETURN_TYPE checkString(string translatedStr)
+        {
+            if(translatedStr.Length > 10)
+            {
+                return RETURN_TYPE.ERROR_LENGTH;
+            }
+            if(translatedStr.Equals(okString))
+            {
+                return RETURN_TYPE.SUCCESS;
+            }
+            return RETURN_TYPE.ERROR;
+        }
+
+    }
+}
